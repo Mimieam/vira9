92a3b70f42a36cb571e401dc6e44357ce23c43c5
Mimieam <t.echismimi@gmail.com>
2015-08-24 09:58:37 -0500

    ios fullscreen, materializecs, game board added

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use(favicon(__dirname + 'images/touch/chrome-touch-icon-192x192.png'));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, '/app')));
+// app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+
+module.exports = app;
