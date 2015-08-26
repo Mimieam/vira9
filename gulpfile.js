6007fd3905e2e6d47c1789fcbfd735e2d3f06043
Mimieam <t.echismimi@gmail.com>
2015-08-25 22:07:01 -0500

    fixed local build

--- gulpfile.js
+++ gulpfile.js
@@ -144,16 +144,21 @@ gulp.task('clean', del.bind(null, ['.tmp', 'dist/*', '!dist/.git'], {dot: true})
 // Watch files for changes & reload
 gulp.task('serve', ['styles'], function () {
   browserSync({
+    proxy: "http://localhost:3000",
+    files: ['.tmp', 'app'],
+    browser: "google chrome",
+    port: 7000,
     notify: false,
     // Customize the BrowserSync console logging prefix
-    logPrefix: 'WSK',
+    logPrefix: 'VI9',
     // Run as an https by uncommenting 'https: true'
     // Note: this uses an unsigned certificate which on first access
     //       will present a certificate warning in the browser.
     // https: true,
-    server: ['.tmp', 'app']
+    // server: ['.tmp', 'app']
   });
 
+  gulp.watch(['server.js'], ['jshint',reload]);
   gulp.watch(['app/**/*.html'], reload);
   gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
   gulp.watch(['app/scripts/**/*.js'], ['jshint']);
@@ -164,7 +169,7 @@ gulp.task('serve', ['styles'], function () {
 gulp.task('serve:dist', ['default'], function () {
   browserSync({
     notify: false,
-    logPrefix: 'WSK',
+    logPrefix: 'VI9',
     // Run as an https by uncommenting 'https: true'
     // Note: this uses an unsigned certificate which on first access
     //       will present a certificate warning in the browser.
@@ -191,3 +196,62 @@ gulp.task('pagespeed', function (cb) {
 
 // Load custom tasks from the `tasks` directory
 // try { require('require-dir')('tasks'); } catch (err) { console.error(err); }
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+
+// we'd need a slight delay to reload browsers
+// connected to browser-sync after restarting nodemon
+var BROWSER_SYNC_RELOAD_DELAY = 500;
+
+gulp.task('nodemon', function (cb) {
+  var called = false;
+  return nodemon({
+
+    // nodemon our expressjs server
+    script: 'bin/www',
+
+    // watch core server file(s) that require server restart on change
+    watch: ['server.js']
+  })
+    .on('start', function onStart() {
+      // ensure start only got called once
+      if (!called) { cb(); }
+      called = true;
+    })
+    .on('restart', function onRestart() {
+      // reload connected browsers after a slight delay
+      setTimeout(function reload() {
+        browserSync.reload({
+          stream: false   //
+        });
+      }, BROWSER_SYNC_RELOAD_DELAY);
+    });
+});
+
+gulp.task('browser-sync', ['default', 'nodemon'], function () {
+
+  // for more browser-sync config options: http://www.browsersync.io/docs/options/
+  browserSync.init({
+
+    // watch the following files; changes will be injected (css & images) or cause browser to refresh
+    files: ['public/**/*.*'],
+
+    // informs browser-sync to proxy our expressjs app which would run at the following location
+    proxy: 'http://localhost:3000',
+
+    // informs browser-sync to use the following port for the proxied app
+    // notice that the default port is 3000, which would clash with our expressjs
+    port: 4000,
+
+    // open the proxied app in chrome
+    browser: ['google-chrome']
+  });
+});
+
+
+// gulp.task('default', ['browser-sync']);
+
--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-var express = require('express');
+var fs = require('fs');
 var path = require('path');
+var express = require('express');
+var favicon = require('serve-favicon');
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
 
-// app.use(favicon(__dirname + 'images/touch/chrome-touch-icon-192x192.png'));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, '/app')));
-// app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use(express.static(path.join(__dirname, '/dist'))); // eventually everything should be served from there
+app.use(favicon(__dirname + '/app/images/touch/chrome-touch-icon-192x192.png'));
+//app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
+/* GET home page. */
+app.get('/raw', function(req, res, next) {
+    console.log("req app", req.hostname, req.app.settings.port);
+    fs.readFile(__dirname + '/../app/index.html', 'utf8', function(err, text){
+        res.send("Hi miezan");
+    });
+});
 
 module.exports = app;
